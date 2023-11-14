251b14db6637afeba3f18028d5da88bd7a8dce45
JonasDoesThings <jonas@jonasdoesthings.com>
2023-11-14 19:38:08 +0100

    style: apply eslint rules

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,42 +1,42 @@
-import { unstable_dev } from "wrangler";
-import type { UnstableDevWorker } from "wrangler";
-import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { unstable_dev } from 'wrangler';
+import type { UnstableDevWorker } from 'wrangler';
+import { describe, expect, it, beforeAll, afterAll } from 'vitest';
 
-describe("Worker", () => {
-	let worker: UnstableDevWorker;
+describe('Worker', () => {
+  let worker: UnstableDevWorker;
 
-	beforeAll(async () => {
-		worker = await unstable_dev("src/index.ts", {
-			experimental: { disableExperimentalWarning: true },
-			vars: {
-				ACCEPTED_API_TOKENS: "justtesting"
-			}
-		});
-	});
+  beforeAll(async () => {
+    worker = await unstable_dev('src/index.ts', {
+      experimental: { disableExperimentalWarning: true },
+      vars: {
+        ACCEPTED_API_TOKENS: 'justtesting',
+      },
+    });
+  });
 
-	afterAll(async () => {
-		await worker.stop();
-	});
+  afterAll(async () => {
+    await worker.stop();
+  });
 
-	it("should resolve the given amazon url", async () => {
-		const requestHeaders = {
-			'Authorization': 'Bearer justtesting'
-		};
+  it('should resolve the given amazon url', async () => {
+    const requestHeaders = {
+      'Authorization': 'Bearer justtesting',
+    };
 
-		const resp = await worker.fetch('?target=https://amzn.to', {headers: requestHeaders});
-		if (resp) {
-			expect(resp.status).toBe(200);
+    const resp = await worker.fetch('?target=https://amzn.to', {headers: requestHeaders});
+    if (resp) {
+      expect(resp.status).toBe(200);
 
-			const respData = await resp.json();
-			expect((respData as {url: string}).url).toStrictEqual("https://www.amazon.com/");
-		}
+      const respData = await resp.json();
+      expect((respData as {url: string}).url).toStrictEqual('https://www.amazon.com/');
+    }
 
-		const resp2 = await worker.fetch('?target=https://amzn.to', {headers: requestHeaders});
-		if (resp2) {
-			expect(resp2.status).toBe(200);
+    const resp2 = await worker.fetch('?target=https://amzn.to', {headers: requestHeaders});
+    if (resp2) {
+      expect(resp2.status).toBe(200);
 
-			const respData = await resp2.json();
-			expect((respData as {url: string}).url).toStrictEqual("https://www.amazon.com/");
-		}
-	});
+      const respData = await resp2.json();
+      expect((respData as {url: string}).url).toStrictEqual('https://www.amazon.com/');
+    }
+  });
 });
